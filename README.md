832db47aa5aceb8fe54ece6e5b0f1f3c32a8891e
unknown <D:\MAIL>
2014-03-06 00:56:22 +0600

    0.0.1

--- README.md
+++ README.md
@@ -2,3 +2,21 @@ caller_info
 ===========
 
 Caller info (file, class, method, line number)
+
+```dart
+import 'package:caller_info/caller_info.dart';
+
+void main() {
+  Foo.test();
+}
+
+class Foo {
+  static void test() {
+    var ci = new CallerInfo();
+    print("file: ${ci.fileName}");
+    print("class: ${ci.className}");
+    print("method: ${ci.methodName}");
+    print("line: ${ci.lineNumber}");
+  }
+}
+```
--- example/example.dart
+++ example/example.dart
@@ -1,39 +1,15 @@
 import 'package:caller_info/caller_info.dart';
 
 void main() {
-  print(new CallerInfo());
-  new Foo();
-  test();
-  var count = 10000;
-  var sw = new Stopwatch();
-  sw.start();
-  for(var i = 0; i < count; i++) {
-    new CallerInfo();
-  }
-
-  sw.stop();
-  var elapsedTime = sw.elapsedMicroseconds;
-  if(elapsedTime > 0) {
-    var throughput = (1000000 / elapsedTime * count).toInt();
-    print("Throughput: $throughput per second");
-  } else {
-  }
+  Foo.test();
 }
 
 class Foo {
-  Foo() {
-    _test();
+  static void test() {
+    var ci = new CallerInfo();
+    print("file: ${ci.fileName}");
+    print("class: ${ci.className}");
+    print("method: ${ci.methodName}");
+    print("line: ${ci.lineNumber}");
   }
-
-  void _test() {
-    print(new CallerInfo());
-  }
-}
-
-void test() {
-  var ci = new CallerInfo();
-  print("file: ${ci.fileName}");
-  print("class: ${ci.className}");
-  print("method: ${ci.methodName}");
-  print("line: ${ci.lineNumber}");
 }
